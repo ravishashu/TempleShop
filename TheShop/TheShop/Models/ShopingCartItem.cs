c1b2027970ff7c44efa05ed3f25ca15885b83576
ravi <sp_ravikiran@yahoo.com>
2023-02-16 19:24:22 +0000

    point

---
+++ TheShop/TheShop/Models/ShopingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TheShop.Models
+{
+    public class ShopingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+        public ShopItem ShopItem { get; set; } = default!;
+        public int Amount { get; set; }
+        public string? ShoppingCartId { get; set; }
+    }
+}
