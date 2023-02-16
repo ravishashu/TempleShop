520066a0288ce9032cccd2277b3236cdf9ebaaae
ravi <sp_ravikiran@yahoo.com>
2023-02-16 18:31:56 +0000

    Routing complete

---
+++ TheShop/TheShop/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheShop.Models;
+using TheShop.ViewModels;
+
+namespace TheShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IShopItemRepository _shopRepository;
+
+        public HomeController(IShopItemRepository pieRepository)
+        {
+            _shopRepository = pieRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var piesOfTheWeek = _shopRepository.ItemsOfTheWeek;
+
+            var homeViewModel = new HomeViewModel(piesOfTheWeek);
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ TheShop/TheShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TheShop.Models;
+
+namespace TheShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<ShopItem> ShopItemOfTheWeek { get; }
+
+        public HomeViewModel(IEnumerable<ShopItem> shopItemOfTheWeek)
+        {
+            ShopItemOfTheWeek = shopItemOfTheWeek;
+        }
+    }
+}
