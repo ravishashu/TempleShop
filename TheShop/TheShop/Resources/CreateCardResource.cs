3791d54952627a947cc269375f6c61878b96abe5
ravi <sp_ravikiran@yahoo.com>
2023-03-01 17:13:06 +0000

    stirp started

---
+++ TheShop/TheShop/Resources/CreateCardResource.cs
@@ -0,0 +1,27 @@
+﻿namespace TheShop.Resources
+{
+    public record CreateCardResource(
+     string Name,
+     string Number,
+     string ExpiryYear,
+     string ExpiryMonth,
+     string Cvc);
+    public record CreateCustomerResource(
+    string Email,
+    string Name,
+    CreateCardResource Card);
+
+    public record CreateChargeResource(
+    string Currency,
+    long Amount,
+    string CustomerId,
+    string ReceiptEmail,
+    string Description);
+    public record ChargeResource(
+    string ChargeId,
+    string Currency,
+    long Amount,
+    string CustomerId,
+    string ReceiptEmail,
+    string Description);
+}
